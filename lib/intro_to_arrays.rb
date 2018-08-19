abfb0d90898efb63f5aabcdbb8e754c94203431f
Olivier Willems <ojwillems@gmail.com>
2018-08-19 01:14:48 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,11 +6,4 @@ def array_with_two_elements
   ["Fuck", "You"]
 end
     
-    describe "#first_element" do
-      it "takes in an argument of an array and returns the first element in the array using its positive index" do
-        my_first_element = first_element(@taylor_swift)
-        expect(my_first_element).to eq("Welcome to New York")
-      end
-    end
-    
 first_element[0]
